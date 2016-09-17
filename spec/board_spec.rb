b2abb2b1181e10966e7c9a77a4fd54bd2c0b2451
Michael Kempster <michaelkempster@bigpond.com>
2016-09-17 16:01:47 +1000

    Added the board tests.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,1 +1,20 @@
-board_spec.rb
+require 'minitest/autorun'
+require 'minitest/reporters'
+require './lib/board.rb'
+
+reporter_options = { color: true }
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
+
+describe Board, "Setting up the board" do
+  before do
+    @board_object = Board.new(5, 5)
+  end
+
+  it "should be of the specified width" do
+    assert @board_object.width == 5
+  end
+
+  it "should be of the specified length" do
+    assert @board_object.length == 5
+  end
+end
