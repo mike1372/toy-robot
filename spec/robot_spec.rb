359068f586a7490ce4f5ca3fb89927f4bc924b2d
Michael Kempster <michaelkempster@bigpond.com>
2016-09-17 15:28:54 +1000

    Made some changes to the robot test.

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -6,35 +6,46 @@ reporter_options = { color: true }
 Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
 
 describe Robot, "Calling commands on the robot" do
-  before do
-    @robot_object = Robot.new
-    @robot_object2 = Robot.new
+  # before do
+  #   @robot_object = Robot.new
+  #   @robot_object2 = Robot.new
+  # end
+
+  it "should report something meaningful if it is not yet placed on the board" do
+		@robot_object = Robot.new
+    assert @robot_object.report == 'Robot is not yet on the board'
+  end
+
+  it "should handle a REPORT command once it is placed" do
+  	@robot_object = Robot.new
+    @robot_object.place([0, 0, 'NORTH'])
+    assert @robot_object.report == '0,0,NORTH'
   end
 
   it "should handle a PLACE command" do
-    @robot_object.place([0, 0, 'N'])
+  	@robot_object = Robot.new
+    @robot_object.place([0, 0, 'NORTH'])
     assert @robot_object.report == '0,0,NORTH'
   end
 
   it "should handle a MOVE command" do
-    @robot_object.place('M')
-    assert @robot_object.report == '0,0,NORTH'
-  end
-  it "should handle a LEFT command" do
-    @robot_object.place('L')
-    assert @robot_object.report == '0,0,NORTH'
-  end
-  it "should handle a RIGHT command" do
-    @robot_object.place('R')
-    assert @robot_object.report == '0,0,NORTH'
+  	@robot_object = Robot.new
+  	@robot_object.place([0, 0, 'NORTH'])
+    @robot_object.move
+    assert @robot_object.report == '0,1,NORTH'
   end
 
-  it "should handle a REPORT command" do
-  	@robot_object.place('Z')
-    assert @robot_object.report == '0,0,NORTH'
+  it "should be able to turn to the left" do
+  	@robot_object = Robot.new
+  	@robot_object.place([0, 0, 'NORTH'])
+    @robot_object.left
+    assert @robot_object.report == '0,0,WEST'
   end
 
-	it "should report something meaningful if it is not yet placed on the board" do
-    assert @robot_object2.report == 'Robot is not yet on the board'
+  it "should be able to turn to the right" do
+  	@robot_object = Robot.new
+  	@robot_object.place([0, 0, 'NORTH'])
+    @robot_object.right
+    assert @robot_object.report == '0,0,EAST'
   end
 end
