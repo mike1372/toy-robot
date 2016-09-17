40c8891148c5a12630b1c9f1c62c35f54ab85053
Michael Kempster <michaelkempster@bigpond.com>
2016-09-17 22:07:29 +1000

    Updated the robot tests.

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -6,44 +6,37 @@ reporter_options = { color: true }
 Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
 
 describe Robot, "Calling commands on the robot" do
-  # before do
-  #   @robot_object = Robot.new
-  #   @robot_object2 = Robot.new
-  # end
+  before do
+    @robot_object = Robot.new
+  end
 
   it "should report something meaningful if it is not yet placed on the board" do
-		@robot_object = Robot.new
     assert @robot_object.report == 'Robot is not yet on the board'
   end
 
   it "should handle a REPORT command once it is placed" do
-  	@robot_object = Robot.new
     @robot_object.place([0, 0, 'NORTH'])
     assert @robot_object.report == '0,0,NORTH'
   end
 
   it "should handle a PLACE command" do
-  	@robot_object = Robot.new
     @robot_object.place([0, 0, 'NORTH'])
     assert @robot_object.report == '0,0,NORTH'
   end
 
   it "should handle a MOVE command" do
-  	@robot_object = Robot.new
   	@robot_object.place([0, 0, 'NORTH'])
     @robot_object.move
     assert @robot_object.report == '0,1,NORTH'
   end
 
-  it "should be able to turn to the left" do
-  	@robot_object = Robot.new
+  it "should be able to handle a LEFT command" do
   	@robot_object.place([0, 0, 'NORTH'])
     @robot_object.left
     assert @robot_object.report == '0,0,WEST'
   end
 
-  it "should be able to turn to the right" do
-  	@robot_object = Robot.new
+  it "should be able to handle a RIGHT command" do
   	@robot_object.place([0, 0, 'NORTH'])
     @robot_object.right
     assert @robot_object.report == '0,0,EAST'
