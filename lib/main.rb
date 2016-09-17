08baafdabc0b9fdd89bd07e88d0a661181207119
Michael Kempster <michaelkempster@bigpond.com>
2016-09-17 22:58:33 +1000

    Added main file and tested - appears OK.

--- lib/main.rb
+++ lib/main.rb
@@ -1,1 +1,11 @@
-main.rb
+require './lib/command.rb'
+require './lib/controller.rb'
+
+command = Command.new
+controller = Controller.new
+
+while true
+	input = gets.chomp
+	commands = command.process(input)
+	controller.run_commands(commands)
+end
