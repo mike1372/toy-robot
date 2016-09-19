155e0d01e70997418a52d4aa1ba42a6123886d84
Michael Kempster <michaelkempster@bigpond.com>
2016-09-19 21:51:56 +1000

    Rubocopped main.rb.

--- lib/main.rb
+++ lib/main.rb
@@ -4,8 +4,8 @@ require './lib/controller.rb'
 command = Command.new
 controller = Controller.new
 
-while true
-	input = gets.chomp
-	commands = command.process(input)
-	controller.run_commands(commands)
+loop do
+  input = gets.chomp
+  commands = command.process(input)
+  controller.run_commands(commands)
 end
